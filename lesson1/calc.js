36f29daa057ddef8c42d9d9825a74797f7d6aa80
Vilatii <vitalii.verich@gmail.com>
2016-09-10 14:52:09 +0300

    My version of calculator

--- lesson1/calc.js
+++ lesson1/calc.js
@@ -1,31 +1,68 @@
-/**
- * Разработать логику работы калькулятора
- * Задача при n заданых чисел и при n-1 знаков, посчитать правильный результат выполнения всех операций
- * Важно соблюдать приоритетность выполнения операций
- */
-var values = [16, 2135657879, 2222, 123, 434, 123, 77];
-var operators = ["+", "+", "*", "/", "-", "+"];
+var values = [16, 123, 454, 6434, 2222, 123, 434];
+var operators = ['+', '*', '-', '+', '*', '/'];
+var i=0;
 var result = 0;
+console.log(values.length);
+console.log(operators.length);
+var mnozh = '*';
+var del = '/';
+var minus = '-';
+            
+var ln1 = values.length;
+var ln2 = operators.length;
+var j = 0;
+var cache = 0;
+var cache1 = 0;
+var cache2 = 0;
+var ln = 0;
 
-/**
- ======================================
- YOUR CODE
- ======================================
- */
+for (var k = 0; k < ln1; k++) {
 
-for (var i = 0; i < operators.length; i++) {
-
+    if (ln1 > ln2) {
+        ln = ln1;
+    } else {
+        ln = ln2;
+    }
+    for (var i = 0; i < ln; i++) {
+        if (operators[i] == mnozh) {
+            cache = values[j] * values[j + 1];
+            alert(values[j] + '*' + values[j + 1] + '=' + cache)
+            values[j] = cache;
+            values.splice(j + 1, 1);
+            operators.splice(i, 1);
+            
+        }
+        if (operators[i] == del) {
+            cache1 = values[j] / values[j + 1];
+            alert(values[j] + '/' + values[j + 1] + '=' + cache1)
+            values[j] = cache1;
+            values.splice(j + 1, 1);
+            operators.splice(i, 1);
+            
+        }
+        if (operators[i] == minus) {
+            cache2 = values[j] - values[j + 1];
+            alert(values[j] + '-' + values[j + 1] + '=' + cache2)
+            values[j] = cache2;
+            values.splice(j + 1, 1);
+            operators.splice(i, 1);
+            
+        }
+        j++
+    }
+    break;
 }
-
-var sum = function (a, b) {
-
-};
-
-var minus = function (a, b) {
-
-};
-
-
-
-
+ln1 = values.length;
+ln2 = operators.length;
+j = 0;
+if (ln1 > ln2) {
+    ln = ln1;
+} else {
+    ln = ln2;
+}
+for (var i = 0; i < ln; i++) {
+    result += values[j];
+    j++
+}
 console.log(result);
+alert("Result: " + result)
