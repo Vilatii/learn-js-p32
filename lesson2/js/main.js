1bbe0b08e1f9726587b4fb2ef6fcdb3f56ea5982
Vilatii <vitalii.verich@gmail.com>
2016-09-18 11:56:43 +0300

    Lens for images

---
+++ lesson2/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#img_01').imageLens({ lensSize: 100 });
+    $('#img_02').imageLens({ lensSize: 200 }); 
+    $('#img_03').imageLens({ lensSize: 150, imageSrc: 'data_images/img5.jpg' }); 
+    $('#img_04').imageLens({ lensSize: 200, borderSize: 8, borderColor: '#fofof' }); 
+});
