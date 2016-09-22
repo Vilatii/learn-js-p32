4ba4c959cdf3fa70e150e957ed9674d790202826
Vilatii <vitalii.verich@gmail.com>
2016-09-22 18:54:00 +0300

    Gallery

---
+++ lesson2/js/gallery.js
@@ -0,0 +1,27 @@
+﻿$(function() {
+        $("img").mouseenter(function() {
+            var
+                self = this,
+                clone = self.__clone = self.__clone || $(self).clone(false);
+ 
+            $(self).before(clone);
+ 
+            clone.css({position: "absolute", "z-index": 9000}).stop().animate({
+                width: "400px",
+                marginLeft: "-50px"
+            },{
+                duration: 80
+            }).mouseleave(function() {
+                clone.stop().animate({
+                    width: "100px",
+                    marginLeft: "0px"
+                }, {
+                    duration: 80,
+                    complete: function() {
+                        $(this).remove();
+                        self.__clone = null;
+                    }
+                });
+            });
+        });
+    });
