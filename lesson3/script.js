9f885d8125534aa4d0b7030d2fa81319dcaad2d0
Vilatii <vitalii.verich@gmail.com>
2016-09-28 17:49:24 +0300

    Social Buttons

---
+++ lesson3/script.js
@@ -0,0 +1,21 @@
+﻿var socialMedia = {
+	<a href="#" class="sb flat twitter">Twitter</a>
+
+	<a href="#" class="sb flat facebook">Facebook</a>
+
+	<a href="#" class="sb flat heart">Heart</a>
+
+	<a href="#" class="sb flat linkedin">LinkedIn</a>
+
+	<a href="#" class="sb flat pinterest">Pinterest</a>
+
+	<a href="#" class="sb flat podcast">Podcast</a>
+
+	<a href="#" class="sb flat rss">RSS</a>
+
+	<a href="#" class="sb flat share_this">Share</a>
+
+	<a href="#" class="sb flat star">Star</a>
+
+	<a href="#" class="sb flat vimeo">Vimeo</a>
+};
